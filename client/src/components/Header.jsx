f54b13b39f99027ea4013e49c1327f2b90d0112d
Samiksha Wankar <samikshawankar808@gmail.com>
2025-01-12 17:49:43 +0530

    Created a Private Route and Outlet for profile & added header update for avatar after successful sign in.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user)
     return (
         <header className="bg-[#0e0e0e] shadow-md">
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -20,7 +21,7 @@ const Header = () => {
                         placeholder="Search..."
                         className="bg-transparent focus:outline-none w-24 sm:w-64"
                     />
-                    <FaSearch className="text-slate-600 hover:text-[#99D3DF]"/>
+                    <FaSearch className="text-slate-600 hover:text-[#99D3DF]" />
                 </form>
 
                 <ul className="flex gap-4 text-white">
@@ -34,10 +35,14 @@ const Header = () => {
                             About
                         </li>
                     </Link>
-                    <Link to="/sign-in">
-                        <li className="hover:underline hover:text-[#88BBD6]">
-                            Sign in
-                        </li>
+                    <Link to="/profile">
+                        {currentUser ? (
+                            <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+                        ) : (
+                            <li className="hover:underline hover:text-[#88BBD6]">
+                                Sign in
+                            </li>
+                        )}
                     </Link>
                 </ul>
             </div>
